b4561d0db30c5e6aeb7254a31545cdcc195bf3ac
Henry Villavicencio <henry@MacBook-Pro-de-Henry.local>
2020-03-18 23:02:48 -0500

    feat:  add static files & style jsx

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,29 @@
 export default () => {
-  return <h1>!Hola mundo!</h1>;
+  return (
+    <div>
+      <img src="/static/initgrammers-logo.png" alt="initgrammers" />
+      <h1>Initgrammers</h1>
+      <p> Pequeñas ideas, gran impacto </p>
+      <style jsx>{`
+        h1 {
+          color: white;
+          text-align: center;
+        }
+        p {
+          color: white;
+          text-align: center;
+        }
+        img {
+          display: block;
+          max-width: 50%;
+          margin: 0 auto;
+        }
+      `}</style>
+      <style jsx global>{`
+        body {
+          background: #a2145b;
+        }
+      `}</style>
+    </div>
+  );
 };
